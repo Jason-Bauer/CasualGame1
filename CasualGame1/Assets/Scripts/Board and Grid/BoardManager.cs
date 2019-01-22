bb872a1681af9c2bef4f3387827d0a10151ea1d1
DESKTOP-QGHGAFT\Jason <jcb5944@g.rit.edu>
2019-01-21 22:26:24 -0500

    Tutorial Started, Swapping, Matching crashes  all of unity, Idk work on it later.

---
+++ CasualGame1/Assets/Scripts/Board and Grid/BoardManager.cs
@@ -0,0 +1,158 @@
+﻿/*
+ * Copyright (c) 2017 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+	public static BoardManager instance;
+	public List<Sprite> characters = new List<Sprite>();
+    public GameObject A, B, C;
+	public GameObject tile;
+	public int xSize, ySize;
+    
+	public GameObject[,] tiles;
+
+	public bool IsShifting { get; set; } 
+
+	void Start () {
+		instance = GetComponent<BoardManager>();
+        
+		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        CreateBoard(offset.x, offset.y);
+    }
+
+	private void CreateBoard (float xOffset, float yOffset) {
+		tiles = new GameObject[xSize, ySize];
+
+        float startX = transform.position.x;
+		float startY = transform.position.y;
+
+        Sprite[] previousLeft = new Sprite[ySize];
+        Sprite previousBelow = null;
+
+
+        for (int X = 0; X < xSize; X++) {
+			for (int Y = 0; Y < ySize; Y++) {
+
+
+				GameObject newTile = Instantiate(tile, new Vector3(startX + ((xOffset+.3f) * X), startY + ((yOffset+.3f) * Y),0), tile.transform.rotation);
+				tiles[X,Y] = newTile;
+                newTile.transform.parent = transform;
+
+                List<Sprite> possibleCharacters = new List<Sprite>(); 
+                possibleCharacters.AddRange(characters); 
+
+                possibleCharacters.Remove(previousLeft[Y]);
+                possibleCharacters.Remove(previousBelow);
+
+                int rand = Random.Range(0, possibleCharacters.Count);
+                Sprite newSprite = possibleCharacters[rand];
+                newTile.GetComponent<Tile>().X = X;
+                newTile.GetComponent<Tile>().Y = Y;
+                newTile.GetComponent<Tile>().type = rand;
+                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+                previousLeft[Y] = newSprite;
+                previousBelow = newSprite;
+
+
+            }
+        }
+    }
+
+    public IEnumerator FindNullTiles()
+    {
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                if (tiles[x, y].GetComponent<SpriteRenderer>().sprite == null)
+                {
+                    yield return StartCoroutine(ShiftTilesDown(x, y));
+                    break;
+                }
+            }
+        }
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                tiles[x, y].GetComponent<Tile>().ClearAllMatches();
+            }
+        }
+
+    }
+
+    private IEnumerator ShiftTilesDown(int x, int yStart, float shiftDelay = .03f)
+    {
+        IsShifting = true;
+        List<SpriteRenderer> renders = new List<SpriteRenderer>();
+        int nullCount = 0;
+
+        for (int y = yStart; y < ySize; y++)
+        {  // 1
+            SpriteRenderer render = tiles[x, y].GetComponent<SpriteRenderer>();
+            if (render.sprite == null)
+            { // 2
+                nullCount++;
+            }
+            renders.Add(render);
+        }
+
+        for (int i = 0; i < nullCount; i++)
+        { // 3
+            yield return new WaitForSeconds(shiftDelay);// 4
+            for (int k = 0; k < renders.Count - 1; k++)
+            { // 5
+                renders[k].sprite = renders[k + 1].sprite;
+                renders[k + 1].sprite = GetNewSprite(x, ySize - 1);
+            }
+        }
+        IsShifting = false;
+    }
+
+    private Sprite GetNewSprite(int x, int y)
+    {
+        List<Sprite> possibleCharacters = new List<Sprite>();
+        possibleCharacters.AddRange(characters);
+
+        if (x > 0)
+        {
+            possibleCharacters.Remove(tiles[x - 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (x < xSize - 1)
+        {
+            possibleCharacters.Remove(tiles[x + 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (y > 0)
+        {
+            possibleCharacters.Remove(tiles[x, y - 1].GetComponent<SpriteRenderer>().sprite);
+        }
+
+        return possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+    }
+
+
+
+
+}
---
+++ CasualGame1/Assets/Scripts/Board and Grid/Tile.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Copyright (c) 2017 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour {
+	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+	private static Tile previousSelected = null;
+    public int X;
+    public int Y;
+    public int type;
+	private SpriteRenderer render;
+	private bool isSelected = false;
+
+	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+    public bool matchFound = false;
+
+    void Awake() {
+		render = GetComponent<SpriteRenderer>();
+    }
+
+	private void Select() {
+		isSelected = true;
+		render.color = selectedColor;
+		previousSelected = gameObject.GetComponent<Tile>();
+		
+	}
+
+	private void Deselect() {
+		isSelected = false;
+		render.color = Color.white;
+		previousSelected = null;
+	}
+
+    void OnMouseDown()
+    {
+        // 1
+        if (render.sprite == null || BoardManager.instance.IsShifting)
+        {
+            return;
+        }
+
+        if (isSelected)
+        { // 2 Is it already selected?
+            Deselect();
+        }
+        else
+        {
+            if (previousSelected == null)
+            { // 3 Is it the first tile selected?
+                Select();
+            }
+            else
+            {
+                SwapSprite(previousSelected.render);
+                previousSelected.ClearAllMatches();
+
+                previousSelected.Deselect(); // 4
+                ClearAllMatches();
+
+
+            }
+        }
+    }
+
+    public void SwapSprite(SpriteRenderer render2)
+    { // 1
+        if (render.sprite == render2.sprite)
+        { // 2
+            return;
+        }
+        int temptype = render2.gameObject.GetComponent<Tile>().type;
+        Sprite tempSprite = render2.sprite; // 3
+        render2.sprite = render.sprite; // 4
+        render2.gameObject.GetComponent<Tile>().type = type;
+        render.sprite = tempSprite; // 5
+        type=render2.gameObject.GetComponent<Tile>().type;
+    }
+
+    private GameObject GetAdjacent(Vector2 castDir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        if (hit.collider != null)
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+
+    private List<GameObject> GetAllAdjacentTiles()
+    {
+        List<GameObject> adjacentTiles = new List<GameObject>();
+        for (int i = 0; i < adjacentDirections.Length; i++)
+        {
+            adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+        }
+        return adjacentTiles;
+    }
+
+    private List<GameObject> FindMatch(Vector2 castDir)
+    { // 1
+        List<GameObject> matchingTiles = new List<GameObject>(); // 2
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir); // 3
+        while (hit.collider != null && hit.collider.GetComponent<SpriteRenderer>().sprite == render.sprite)
+        { // 4
+            matchingTiles.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, castDir);
+        }
+        return matchingTiles; // 5
+    }
+
+    private void ClearMatch(Vector2[] paths) // 1
+    {
+        List<GameObject> matchingTiles = new List<GameObject>(); // 2
+        for (int i = 0; i < paths.Length; i++) // 3
+        {
+            matchingTiles.AddRange(FindMatch(paths[i]));
+        }
+        if (matchingTiles.Count >= 2) // 4
+        {
+            for (int i = 0; i < matchingTiles.Count; i++) // 5
+            {
+                matchingTiles[i].GetComponent<SpriteRenderer>().sprite = null;
+            }
+            matchFound = true; // 6
+        }
+    }
+
+    public void ClearAllMatches()
+    {
+        if (render.sprite == null)
+            return;
+
+        ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
+        ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+        if (matchFound)
+        {
+            render.sprite = null;
+            matchFound = false;
+            StopCoroutine(BoardManager.instance.FindNullTiles());
+            StartCoroutine(BoardManager.instance.FindNullTiles());
+
+        }
+    }
+
+
+
+}
