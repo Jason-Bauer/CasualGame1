b54b78e990d54e0f2bffcdc09041f8a95b55554c
DESKTOP-QGHGAFT\Jason <jcb5944@g.rit.edu>
2019-01-25 00:04:16 -0500

    unfaffed some more code and added more character sprites

--- CasualGame1/Assets/Scripts/Board and Grid/BoardManager.cs
+++ CasualGame1/Assets/Scripts/Board and Grid/BoardManager.cs
@@ -1,24 +1,4 @@
-﻿/*
- * Copyright (c) 2017 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+﻿
 
 using UnityEngine;
 using System.Collections;
@@ -92,9 +72,17 @@ public class BoardManager : MonoBehaviour {
                 {
                     newTile.GetComponent<Tile>().type = 4;
                 }
-                else 
+                else if (newTile.GetComponent<SpriteRenderer>().sprite.name.ToString() == "f")
                 {
                     newTile.GetComponent<Tile>().type = 5;
+                }
+                else if (newTile.GetComponent<SpriteRenderer>().sprite.name.ToString() == "g")
+                {
+                    newTile.GetComponent<Tile>().type = 6;
+                }
+                else 
+                {
+                    newTile.GetComponent<Tile>().type = 7;
                 }
 
 
@@ -114,7 +102,7 @@ public class BoardManager : MonoBehaviour {
             {
                 if (tiles[x, y].GetComponent<SpriteRenderer>().sprite == null)
                 {
-                    Debug.Log("Shift Attempt");
+                    //Debug.Log("Shift Attempt");
                     ShiftDown(x, x, y);
                 }
             }
@@ -169,9 +157,17 @@ public class BoardManager : MonoBehaviour {
 			{
 				tiles[tileCur.X,tileCur.Y].GetComponent<Tile>().type = 4;
 			}
-			else 
+            else if (tiles[tileCur.X, tileCur.Y].GetComponent<SpriteRenderer>().sprite.name.ToString() == "f")
+            {
+                tiles[tileCur.X, tileCur.Y].GetComponent<Tile>().type = 5;
+            }
+            else if (tiles[tileCur.X, tileCur.Y].GetComponent<SpriteRenderer>().sprite.name.ToString() == "g")
+            {
+                tiles[tileCur.X, tileCur.Y].GetComponent<Tile>().type = 6;
+            }
+            else 
 			{
-				tiles[tileCur.X,tileCur.Y].GetComponent<Tile>().type = 5;
+				tiles[tileCur.X,tileCur.Y].GetComponent<Tile>().type = 7;
 			}
 		}
 
@@ -179,70 +175,6 @@ public class BoardManager : MonoBehaviour {
 
 
     }
-
-    public void SwapSprite(SpriteRenderer render2, SpriteRenderer render)
-    { // 1
-        if (render.sprite == render2.sprite)
-        { // 2
-            return;
-        }
-        int temptype = render2.gameObject.GetComponent<Tile>().type;
-        Sprite tempSprite = render2.sprite; // 3
-        render2.sprite = render.sprite; // 4
-        render2.gameObject.GetComponent<Tile>().type = render.gameObject.GetComponent<Tile>().type;
-        //render.sprite = tempSprite; // 5
-        //render.gameObject.GetComponent<Tile>().type = temptype;
-    }
-
-    private IEnumerator ShiftTilesDown(int x, int yStart, float shiftDelay = .03f)
-    {
-        IsShifting = true;
-        List<SpriteRenderer> renders = new List<SpriteRenderer>();
-        int nullCount = 0;
-
-        for (int y = yStart; y < ySize; y++)
-        {  // 1
-            SpriteRenderer render = tiles[x, y].GetComponent<SpriteRenderer>();
-            if (render.sprite == null)
-            { // 2
-                nullCount++;
-            }
-            renders.Add(render);
-        }
-
-        for (int i = 0; i < nullCount; i++)
-        { // 3
-            yield return new WaitForSeconds(shiftDelay);// 4
-            for (int k = 0; k < renders.Count - 1; k++)
-            { // 5
-                renders[k].sprite = renders[k + 1].sprite;
-                renders[k + 1].sprite = GetNewSprite(x, ySize-1 );
-            }
-        }
-        IsShifting = false;
-    }
-
-    private Sprite GetNewSprite(int x, int y)
-    {
-        List<Sprite> possibleCharacters = new List<Sprite>();
-        possibleCharacters.AddRange(characters);
-
-        if (x > 0)
-        {
-            possibleCharacters.Remove(tiles[x - 1, y].GetComponent<SpriteRenderer>().sprite);
-        }
-        if (x < xSize - 1)
-        {
-            possibleCharacters.Remove(tiles[x + 1, y].GetComponent<SpriteRenderer>().sprite);
-        }
-        if (y > 0)
-        {
-            possibleCharacters.Remove(tiles[x, y - 1].GetComponent<SpriteRenderer>().sprite);
-        }
-
-        return possibleCharacters[Random.Range(0, possibleCharacters.Count)];
-    }
-   
 
 
     //don't try and look at the code past this point
@@ -313,8 +245,7 @@ public class BoardManager : MonoBehaviour {
         foreach (GameObject t in tiles)
         {
             if (t.GetComponent<Tile>().matchFound)
-            {
-				
+            {	
                 Destroytile(t);
                 callcheck = true;
             }
@@ -327,33 +258,6 @@ public class BoardManager : MonoBehaviour {
 
     public void Destroytile(GameObject t)
     {
-		
         t.GetComponent<SpriteRenderer>().sprite = null;
-
     }
-     public List<GameObject> adjacenttiles(int x, int y)
-    {
-        List<GameObject> returntiles=new List<GameObject>();
-        
-		if (x > 0 && tiles[x - 1, y].GetComponent<Tile>().type == tiles[x, y].GetComponent<Tile>().type)
-        {
-            returntiles.Add(tiles[x - 1, y]);
-        }
-		if (x < xSize &&tiles[x + 1, y].GetComponent<Tile>().type == tiles[x, y].GetComponent<Tile>().type)
-        {
-            returntiles.Add(tiles[x + 1, y]);
-        }
-		if (y > 0 && tiles[x, y - 1].GetComponent<Tile>().type == tiles[x, y].GetComponent<Tile>().type)
-        {
-            returntiles.Add(tiles[x , y-1]); 
-        }
-		if (y < ySize && tiles[x, y + 1].GetComponent<Tile>().type == tiles[x, y].GetComponent<Tile>().type)
-        {
-            returntiles.Add(tiles[x , y+1]);
-        }
-
-        return returntiles;
-    }
-
-
 }
